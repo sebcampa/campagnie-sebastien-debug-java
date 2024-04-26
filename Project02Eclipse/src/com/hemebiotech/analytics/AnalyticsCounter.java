f674bec361e15517367236e21c205e96c1220971
sebcampa <sebastien_campagnie@yahoo.com>
2024-04-26 17:08:17 +0200

    Fixing existing code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,40 +3,68 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+	private static int symptomCount = 0;
+
+	public static <lineSymptom> void main(String args[]) throws Exception {
+        final String path = ".\\Project02Eclipse\\";
+		String fileToRead = "symptoms.txt";
+        System.out.println(Paths.get(path + fileToRead).toAbsolutePath());
+
+		ReadSymptomDataFromFile symptomsFile = new ReadSymptomDataFromFile(path + fileToRead);
+		System.out.println(symptomsFile.GetSymptoms());
+		//System.out.println(Paths.get("").toAbsolutePath());
+		Map<String,Integer> symptomsMap = new HashMap<>();
+		BufferedReader reader = new BufferedReader (new FileReader(path + fileToRead));
 		String line = reader.readLine();
 
 		int i = 0;
+		String lineDelimiterForSplit = ":";
 		int headCount = 0;
 		while (line != null) {
 			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+			line = reader.readLine();
+			//lineSymptom = line.split(lineDelimiterForSplit)[0];
+			//System.out.println("symptom from file: " + lineSymptom);
+			if (!(symptomsMap.containsKey(line)) && line != null) {
+				//headCount++;
+				System.out.println("symptom " + line + " does not exist, let's add it to initializing to 1");
+				symptomsMap.put(line, 1);
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+			else if (line != null){
+				int currentSymptomCount = symptomsMap.get(line);
+				System.out.println("symptom " + line + " is already in Map with value" + currentSymptomCount);
+				symptomsMap.put(line, currentSymptomCount + 1);
 			}
 
-			line = reader.readLine();
 		}
-		
+		reader.close();
+//		System.out.println(symptomsMap);
+//		symptomsMap.entrySet().stream()
+//				.sorted(Map.Entry.comparingByKey())
+//				.forEach(System.out::println);
+//		for(Map.Entry m:symptomsMap.entrySet()){
+//			System.out.println(m.getKey()+ " : " +m.getValue());}
+		Map<String,Integer> symptomsSortedMap = new TreeMap<String,Integer>(symptomsMap);
+		System.out.println(symptomsSortedMap);
+
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		symptomsMap.entrySet().stream()
+				.sorted(Map.Entry.comparingByKey())
+				.forEach(System.out::println);
+		//writer.write("headache: " + headacheCount + "\n");
+		//writer.write("rash: " + rashCount + "\n");
+		//writer.write("dialated pupils: " + pupilCount + "\n");
+		for(Map.Entry m:symptomsSortedMap.entrySet()){
+			writer.write(m.getKey()+ " : " +m.getValue() + String.format("%n"));
+		}
+
 		writer.close();
 	}
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -0,0 +1,4 @@
+package com.hemebiotech.analytics;
+
+public interface ISymptomWriter {
+}
